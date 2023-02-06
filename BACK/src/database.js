9a10d8572488aafd7ed4a78251d66e06a852abc3
Guillermo <gcpadilla@gmail.com>
2023-02-06 11:34:10 -0300

    mongo conect

--- BACK/src/database.js
+++ BACK/src/database.js
@@ -7,5 +7,5 @@ mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-	console.log("mongo se conecto ok...");
+	console.log("mongo is conect...");
 });
